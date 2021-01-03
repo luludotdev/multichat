49462d842324434a712e67e01102b0efc27fa16a
Jack Baron <jackmbaron@gmail.com>
2021-01-03 01:04:29 +0000

    join channels on page load

--- lib/hooks/useChat/gateway.ts
+++ lib/hooks/useChat/gateway.ts
@@ -1,4 +1,6 @@
 import { Client as createClient } from 'tmi.js'
+import { IS_SERVER } from '~constants'
+import { sendSystemMessage } from './system'
 
 export const client = createClient({
   connection: {
@@ -11,14 +13,15 @@ export const client = createClient({
   },
 })
 
-void client.connect().then(() => {
-  // TODO: Connect to channels
-  // const channels = window.location.pathname.split('/').filter(Boolean)
-  // for (const channel of channels) {
-  //   client.join(channel).then(chls => {
-  //     for (const ch of chls) {
-  //       sendSystemMessage(`Joined ${ch}`)
-  //     }
-  //   })
-  // }
-})
+if (IS_SERVER === false) {
+  void client.connect().then(() => {
+    const channels = window.location.pathname.split('/').filter(Boolean)
+    for (const channel of channels) {
+      void client.join(channel).then(chls => {
+        for (const ch of chls) {
+          sendSystemMessage(`Joined ${ch}`)
+        }
+      })
+    }
+  })
+}
